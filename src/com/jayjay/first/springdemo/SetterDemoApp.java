4e44d4e32719bad2e39b61b42e57d21ebe6f714f
Jorge Javier <jacksparrow2017.11.30@gmail.com>
2020-05-19 08:25:22 -0400

    :sparkles: feat(Spring Dependency Injection): Dependency Injection by setter. Close #1

--- src/com/jayjay/first/springdemo/SetterDemoApp.java
+++ src/com/jayjay/first/springdemo/SetterDemoApp.java
@@ -2,19 +2,18 @@ package com.jayjay.first.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class SpringHelloApp {
+public class SetterDemoApp {
     public static void main(String[] args) {
 
-        // load the Spring configuration file
+        // load the Spring config file
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // retrieve bean from Spring container
-        Coach theCoach = context.getBean("myCoach", Coach.class);
+        // retrieve the bean from Spring container
+        Coach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
-        // use the object
+        // call methods on the bean
         System.out.println(theCoach.getDailyWorkout());
 
-        // call new method for fortunes
         System.out.println(theCoach.getDailyFortune());
 
         // close the context
